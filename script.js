c8d86c9d2f0a3956abb8655667ff79e6fcf17b2b
greencycle5 <greencycle2k24@gmail.com>
2024-10-30 19:29:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Exibe o post do blog selecionado (simulação para demo)
+function showPost(postId) {
+    alert(O Dr. Carl Jones é um conservacionista mundialmente renomado e
+         um dedicado defensor da vida selvagem, cujo trabalho ao longo da vida 
+         deixou uma marca indelével no campo da biologia da conservação. 
+         Com uma paixão inabalável por preservar as espécies mais ameaçadas do mundo, 
+         o Dr. Jones conquistou uma reputação como um verdadeiro campeão da
+          biodiversidade e um farol de esperança para a vida selvagem em perigo.
+
+    )
+// Simulação de envio de formulário (ouvidoria)
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Obrigado pelo seu feedback!");
+});
